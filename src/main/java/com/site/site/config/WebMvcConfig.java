afead9574b5f3e0cf9de77de2c3fa547e2c4bd1c
Hyeonmo <khm052678@gmail.com>
2022-10-30 00:46:08 +0900

    add : addResourceHandlers

---
+++ src/main/java/com/site/site/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.site.site.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
+    }
+}
